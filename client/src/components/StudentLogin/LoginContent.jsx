2a613bd69253616447b2448367feef4549a8e0ea
Rkenneth13 <it.russelkennethregala@gmail.com>
2023-06-11 08:33:00 +0800

    student login page

--- client/src/components/StudentLogin/LoginContent.jsx
+++ client/src/components/StudentLogin/LoginContent.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Box } from "@mui/material";
+import { Box, SwipeableDrawer } from "@mui/material";
 import { Space, Drawer } from "antd";
 import useStyles from "./style";
 import LoginForm from "./LoginForm";
@@ -19,7 +19,9 @@ const LoginContent = (props) => {
   };
 
   return (
+    
     <Box className={classes.loginContainer}>
+    
       <LoginForm showSignUpForm={showSignUpForm} LoginValid={LoginValid}/>
       <Drawer
         title="Sign Up"
@@ -33,6 +35,22 @@ const LoginContent = (props) => {
       >
         <SignUp onClose={onClose}/>
       </Drawer>
+      
+      <Box className={classes.loginbox}>
+       <Box className={classes.line}>
+       <Box className={classes.loginLabel}>
+        PAMPANGA HIGH SCHOOL               
+       
+        <Box className={classes.loginLabel1}>
+        CITY OF SAN FERNANDO
+        
+       </Box>
+       </Box>
+        <Box className={classes.line1}>
+        <Box className={classes.loginlogo}></Box>
+        </Box>
+        </Box>
+        </Box>
     </Box>
   );
 };
--- client/src/components/StudentLogin/style.js
+++ client/src/components/StudentLogin/style.js
@@ -5,12 +5,14 @@ const theme = createTheme();
 const useStyles = makeStyles(() => ({
   
   loginContainer: {
-    backgroundImage: "url(/login.png)",
+    backgroundImage: "url(/backgound.png)",
     backgroundPosition: "center",
     backgroundSize: "cover",
     minHeight: "100vh",
     display: "flex",
     alignItems: "center",
+    zIndex: -1,
+    
 
     [theme.breakpoints.up("md")]: {
       paddingRight: "100px",
@@ -20,6 +22,32 @@ const useStyles = makeStyles(() => ({
       paddingLeft: "0px",
       justifyContent: "center",
     },
+  },
+
+   loginlogo: {
+   backgroundImage: "url(/logo.png)",
+   height: "50vh",
+   backgroundRepeat: "no-repeat",
+   marginLeft:"-41vh",
+   marginBlockStart: "-35vh",
+   alignItems: "center",
+   zIndex: -1,
+   
+   [theme.breakpoints.up("md")]: {
+    width: "400px",
+  },
+  [theme.breakpoints.down("md")]: {
+    width: "80%",
+    backgroundImage: "url(/logo.png)",
+   height: "50vh",
+   opacity: 0.2,
+   backgroundRepeat: "no-repeat",
+   marginLeft:"73vh",
+   marginBlockStart: "-35vh",
+   alignItems: "center",
+   zIndex: 1,
+    
+  },
   },
   loginCard: {
     display: "flex",
@@ -31,14 +59,110 @@ const useStyles = makeStyles(() => ({
     background: "white",
     border: "1px lightgray solid",
     borderRadius: "10px",
+    zIndex: 1,
 
     [theme.breakpoints.up("md")]: {
       width: "400px",
     },
     [theme.breakpoints.down("md")]: {
       width: "80%",
+
     },
   },
+
+  loginbox:{
+    position: "absolute",
+    borderRadius: "40px",
+    height: "40vh",
+    marginBlockEnd: "10vh",
+    marginRight:"60vh",
+    background: "rgba(236, 234, 219, 0.25)",
+    minWidth: "130vh",
+    [theme.breakpoints.up("md")]: {
+    width: "100px",
+    },
+    [theme.breakpoints.down("md")]: {
+     width: "80vh",
+     background: "none",
+    },
+    },
+  line:{
+  position: "absolute",
+  borderRadius: "12px",
+  height: "2vh",
+  marginBlockStart: "5vh",
+  marginLeft:"40vh",
+  background: "yellow",
+  minWidth: "80vh",
+  zIndex: 1,
+    [theme.breakpoints.up("md")]: {
+      width: "400px",
+    },
+    [theme.breakpoints.down("md")]: {
+      width: "81%",
+      background: "none",
+      marginLeft:"0vh",
+    },
+  },
+
+  loginLabel:{
+    color: "white",
+    fontFamily:"TimesNew Roman",
+    position: "absolute",
+    borderRadius: "12px",
+    height: "2vh",
+    marginBlockStart: "40px",
+    marginLeft:"5vh",
+    fontSize: "45px",
+    minWidth: "100vh",
+    zIndex: 1,
+      [theme.breakpoints.up("md")]: {
+        width: "400px",
+      },
+      [theme.breakpoints.down("md")]: {
+        width: "81%",
+        background: "none",
+        marginLeft:"0vh",
+        fontSize: "0px",
+      },
+    },
+    loginLabel1:{
+      fontFamily:"TimesNew Roman",
+      position: "absolute",
+      borderRadius: "12px",
+      height: "2vh",
+      marginBlockStart: "-1vh",
+      marginLeft:"1vh",
+      fontSize: "45px",
+      minWidth: "100vh",
+      zIndex: 1,
+        [theme.breakpoints.up("md")]: {
+          width: "400px",
+        },
+        [theme.breakpoints.down("md")]: {
+          width: "81%",
+          background: "none",
+          marginLeft:"0vh",
+          fontSize: "0px",
+        },
+      },
+  line1:{
+    position: "absolute",
+    borderRadius: "12px",
+  height: "2vh",
+  marginBlockStart: "30vh",
+  marginLeft:"0vh",
+  background: "yellow",
+  minWidth: "80vh",
+
+  [theme.breakpoints.up("md")]: {
+    width: "400px",
+  },
+  [theme.breakpoints.down("md")]: {
+    width: "81%",
+    background: "none",
+  },
+    },
   loginDetails: {
     display: "flex",
     alignContent: "center",
