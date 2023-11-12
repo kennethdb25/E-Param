94717a227d3d306b10c24d3898d3ca26d178776a
Kenneth <baukenn@mfcgd.com>
2023-11-12 10:45:59 +0800

    FINAL CONFIG

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const port = 5000; // server port
+const port = process.env.PORT || 5000; // server port
 //ROUTES IMPORT
 const SignInRouter = require('./routes/loginRoute/loginRoute');
 const StudentRegRouter = require('./routes/signUpRoute/studentRegRoute');
