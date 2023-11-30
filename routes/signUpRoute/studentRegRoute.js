bacb39830f287734c248f17235c5d9eeda3aad1a
Kenneth <baukenn@mfcgd.com>
2023-11-30 19:18:00 +0800

    FINAL CONFIG

--- routes/signUpRoute/studentRegRoute.js
+++ routes/signUpRoute/studentRegRoute.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const multer = require("multer");
+const express = require('express');
+const multer = require('multer');
 const StudentRegRouter = new express.Router();
-const StudentModel = require("../../models/studentModel");
-const QRCode = require("qrcode");
-const GradeSectionModel = require("../../models/gradeSectionModel");
+const StudentModel = require('../../models/studentModel');
+const QRCode = require('qrcode');
+const GradeSectionModel = require('../../models/gradeSectionModel');
 
 const imgconfig = multer.diskStorage({
   destination: (req, file, callback) => {
-    callback(null, "./uploads");
+    callback(null, './uploads');
   },
   filename: (req, file, callback) => {
     callback(null, `${Date.now()}-${file.originalname}`);
@@ -15,10 +15,10 @@ const imgconfig = multer.diskStorage({
 });
 
 const isImage = (req, file, callback) => {
-  if (file.mimetype.startsWith("image")) {
+  if (file.mimetype.startsWith('image')) {
     callback(null, true);
   } else {
-    callback(new Error("Only image is allowed"));
+    callback(new Error('Only image is allowed'));
   }
 };
 
@@ -28,26 +28,24 @@ const upload = multer({
 });
 
 // register student
-StudentRegRouter.post(
-  "/student/register",
-  upload.single("photo"),
-  async (req, res) => {
-    const { filename } = req.file;
-    const {
-      studentId,
-      firstName,
-      middleName,
-      lastName,
-      address,
-      grade,
-      userType,
-      section,
-      gender,
-      email,
-      password,
-      confirmPassword,
-    } = req.body;
+StudentRegRouter.post('/student/register', upload.single('photo'), async (req, res) => {
+  const { filename } = req.file;
+  const {
+    studentId,
+    firstName,
+    middleName,
+    lastName,
+    address,
+    grade,
+    userType,
+    section,
+    gender,
+    email,
+    password,
+    confirmPassword,
+  } = req.body;
 
+  try {
     // validate if student id exist
     const validate = await StudentModel.findOne({ studentId: studentId });
     const validateEmail = await StudentModel.findOne({ email: email });
@@ -55,20 +53,18 @@ StudentRegRouter.post(
     const acctualCount = countNum + 1;
 
     if (validateEmail && validate) {
-      return res
-        .status(422)
-        .json({ error: "Email and Student ID are already exists" });
+      return res.status(422).json({ error: 'Email and Student ID are already exists' });
     } else if (validateEmail) {
-      return res.status(422).json({ error: "Email is already exists" });
+      return res.status(422).json({ error: 'Email is already exists' });
     } else if (validate) {
-      return res.status(422).json({ error: "Student ID is already exists" });
+      return res.status(422).json({ error: 'Student ID is already exists' });
     }
 
     function findTotalCount(str) {
       let count = 0;
 
       for (let ch of str) {
-        if (ch >= "0" && ch <= "9") {
+        if (ch >= '0' && ch <= '9') {
           count++;
         }
       }
@@ -101,43 +97,41 @@ StudentRegRouter.post(
       num = `000${acctualCount}`;
     }
 
-    try {
-      const qrCode = await QRCode.toDataURL(studentId);
-      const finalUser = new StudentModel({
-        libraryCardNum: num,
-        studentId: studentId.toString().toUpperCase(),
-        firstName: firstName.toUpperCase(),
-        middleName: middleName.toUpperCase(),
-        lastName: lastName.toUpperCase(),
-        address: address.toUpperCase(),
-        grade: grade ? grade : "N/A",
-        section: section ? section : "N/A",
-        imgpath: filename,
-        userType: userType ? userType : "Student",
-        acctStatus: userType !== "Student" ? "Active" : "Pending",
-        QRCode: qrCode,
-        created: new Date().toISOString(),
-        gender,
-        email,
-        password,
-        confirmPassword,
-      });
+    const qrCode = await QRCode.toDataURL(studentId);
+    const finalUser = new StudentModel({
+      libraryCardNum: num,
+      studentId: studentId.toString().toUpperCase(),
+      firstName: firstName.toUpperCase(),
+      middleName: middleName.toUpperCase(),
+      lastName: lastName.toUpperCase(),
+      address: address.toUpperCase(),
+      grade: grade ? grade : 'N/A',
+      section: section ? section : 'N/A',
+      imgpath: filename,
+      userType: userType ? userType : 'Student',
+      acctStatus: userType ? 'Active' : 'Pending',
+      QRCode: qrCode,
+      created: new Date().toISOString(),
+      gender,
+      email,
+      password,
+      confirmPassword,
+    });
 
-      const storeData = await finalUser.save();
+    const storeData = await finalUser.save();
 
-      return res.status(201).json({ status: 201, body: storeData });
-    } catch (error) {
-      console.log(error);
-      return res.status(422).json(error);
-    }
+    return res.status(201).json({ status: 201, body: storeData });
+  } catch (error) {
+    console.log(error);
+    return res.status(422).json(error);
   }
-);
+});
 
 // GET OTHER ACCOUNTS
-StudentRegRouter.get("/other/accounts", async (req, res) => {
+StudentRegRouter.get('/other/accounts', async (req, res) => {
   try {
     const otherAccounts = await StudentModel.find({
-      userType: { $ne: "Student" },
+      userType: { $ne: 'Student' },
     }).sort({ lastName: -1 });
     return res.status(200).json({ status: 200, body: otherAccounts });
   } catch (error) {
@@ -146,7 +140,7 @@ StudentRegRouter.get("/other/accounts", async (req, res) => {
 });
 
 // GET PENDING STUDENT ACCOUNT
-StudentRegRouter.get("/student/pending", async (req, res) => {
+StudentRegRouter.get('/student/pending', async (req, res) => {
   try {
     const pendingAccounts = await StudentModel.find().sort({ created: -1 });
     return res.status(200).json({ status: 200, body: pendingAccounts });
@@ -157,7 +151,7 @@ StudentRegRouter.get("/student/pending", async (req, res) => {
 });
 
 // APPROVE PENDING ACCOUNT
-StudentRegRouter.patch("/student/approve/:_id", async (req, res) => {
+StudentRegRouter.patch('/student/approve/:_id', async (req, res) => {
   try {
     const id = req.params._id;
 
@@ -166,7 +160,7 @@ StudentRegRouter.patch("/student/approve/:_id", async (req, res) => {
     if (!getPendingAccount) {
       return res.status(422).json({ error: `No account match` });
     } else {
-      getPendingAccount.acctStatus = "Active";
+      getPendingAccount.acctStatus = 'Active';
 
       const approveAccount = await getPendingAccount.save();
 
@@ -179,8 +173,8 @@ StudentRegRouter.patch("/student/approve/:_id", async (req, res) => {
 });
 
 //GET STUDENT INFO FOR PROCESSING RESERVED BOOK
-StudentRegRouter.get("/student/get-info", async (req, res) => {
-  const info = req.query.studentId || "";
+StudentRegRouter.get('/student/get-info', async (req, res) => {
+  const info = req.query.studentId || '';
   try {
     const getStudentInfo = await StudentModel.findOne({ studentId: info });
     return res.status(200).json({ status: 200, body: getStudentInfo });
@@ -190,20 +184,20 @@ StudentRegRouter.get("/student/get-info", async (req, res) => {
   }
 });
 
-StudentRegRouter.get("/get-section", async (req, res) => {
-  const grade = req.query.grade || "";
+StudentRegRouter.get('/get-section', async (req, res) => {
+  const grade = req.query.grade || '';
   try {
     const students = await GradeSectionModel.aggregate([
       {
         $match: {
-          status: "Active",
+          status: 'Active',
         },
       },
       {
         $group: {
-          _id: "$grade",
+          _id: '$grade',
           uniqueValues: {
-            $addToSet: "$section",
+            $addToSet: '$section',
           },
         },
       },
@@ -220,7 +214,7 @@ StudentRegRouter.get("/get-section", async (req, res) => {
   }
 });
 
-StudentRegRouter.post("/add-section", async (req, res) => {
+StudentRegRouter.post('/add-section', async (req, res) => {
   const { grade, section } = req.body;
   try {
     const getSectionExist = await GradeSectionModel.find({
@@ -230,14 +224,14 @@ StudentRegRouter.post("/add-section", async (req, res) => {
     const getSectionCount = await GradeSectionModel.find().count();
 
     if (getSectionExist.length > 0) {
-      return res.status(422).json({ error: "Section already exists " });
+      return res.status(422).json({ error: 'Section already exists ' });
     }
 
     const finalRecord = await new GradeSectionModel({
       sectionId: getSectionCount + 1,
       grade,
       section: section.toUpperCase(),
-      status: "Active",
+      status: 'Active',
     });
 
     const storeData = await finalRecord.save();
@@ -249,7 +243,7 @@ StudentRegRouter.post("/add-section", async (req, res) => {
   }
 });
 
-StudentRegRouter.get("/get-all-section", async (req, res) => {
+StudentRegRouter.get('/get-all-section', async (req, res) => {
   try {
     const getAllSection = await GradeSectionModel.find();
     return res.status(200).json({ status: 200, body: getAllSection });
@@ -259,15 +253,15 @@ StudentRegRouter.get("/get-all-section", async (req, res) => {
   }
 });
 
-StudentRegRouter.patch("/change-status-section", async (req, res) => {
-  const id = req.query.sectionId || "";
+StudentRegRouter.patch('/change-status-section', async (req, res) => {
+  const id = req.query.sectionId || '';
   try {
     const getSection = await GradeSectionModel.findOne({ sectionId: id });
 
     if (!getSection) {
-      return res.status(404).json({ error: "Section not found " });
+      return res.status(404).json({ error: 'Section not found ' });
     }
-    getSection.status = getSection.status === "Active" ? "Inactive" : "Active";
+    getSection.status = getSection.status === 'Active' ? 'Inactive' : 'Active';
 
     const changeSuccess = await getSection.save();
     return res.status(200).json({ status: 200, body: changeSuccess });
